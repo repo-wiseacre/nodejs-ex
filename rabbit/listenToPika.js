baa43ec192e6dc5d7bf9d89ceac5ab25660feab3
Shuvajit Ghosh <ghosh.shuvajit@gmail.com>
2020-07-26 21:54:16 +0530

    Update listenToPika.js

--- rabbit/listenToPika.js
+++ rabbit/listenToPika.js
@@ -3,69 +3,73 @@ var fs     = require('fs');
 //console.log("------------------------------"+process.env.CLOUDAMQP_URI+"-----------------------");
 var amqpConn = null;
 
-function start(queue_name, messagestr) {
-  amqp.connect(process.env.CLOUDAMQP_URI, function(error, connection) {
-    if (err) {
-      console.error("[AMQP]", err.message);
-      return setTimeout(start, 1000);
-    }
-    conn.on("error", function(err) {
-      if (err.message !== "Connection closing") {
-        console.error("[AMQP] conn error", err.message);
+function listenToPika() {
+  this.start = function(queue_name, messagestr) {
+    amqp.connect(process.env.CLOUDAMQP_URI, function(error, connection) {
+      if (err) {
+        console.error("[AMQP]", err.message);
+        return setTimeout(start, 1000);
       }
-    });
-    conn.on("close", function() {
-      console.error("[AMQP] reconnecting");
-      return setTimeout(start, 1000);
-    });
-    console.log("[AMQP] connected");
-    
-  });
-  
-  amqpConn = connection;
-  whenConnected(queue_name,messagestr);
-}
-
-function whenConnected(queue_name,messagestr) {
-  startConsumer(queue_name,messagestr);
-}
-
-function startConsumer(queue_name,messagestr){
-  
-  connection.createChannel(function(error1, channel) {
-      
-      let queue = queue_name; //'receiveAPIResponse'; //'callAPIRequest';
-      let msg   = messagestr;//'start_calling_api';
-
-      channel.assertQueue(queue, {
-        durable: true
+      conn.on("error", function(err) {
+        if (err.message !== "Connection closing") {
+          console.error("[AMQP] conn error", err.message);
+        }
       });
-      if (closeOnErr(error1)) return;
-
-      channel.consume(queue, Buffer.from(msg), {
-        persistent: true
+      conn.on("close", function() {
+        console.error("[AMQP] reconnecting");
+        return setTimeout(start, 1000);
       });
-      console.log("Sent '%s'", msg);
-      fs.writeFile('../data/11001101', msg, function (err) {
-        if (err) throw err;               console.log('Results Received');
-      }); 
+      console.log("[AMQP] connected");
+
     });
-    setTimeout(function() {
-      connection.close();
-      process.exit(0)
-    }, 500);  
+
+    amqpConn = connection;
+    whenConnected(queue_name,messagestr);
+  }
+
+  function whenConnected(queue_name,messagestr) {
+    startConsumer(queue_name,messagestr);
+  }
+
+  function startConsumer(queue_name,messagestr){
+
+    connection.createChannel(function(error1, channel) {
+
+        let queue = queue_name; //'receiveAPIResponse'; //'callAPIRequest';
+        let msg   = messagestr;//'start_calling_api';
+
+        channel.assertQueue(queue, {
+          durable: true
+        });
+        if (closeOnErr(error1)) return;
+
+        channel.consume(queue, Buffer.from(msg), {
+          persistent: true
+        });
+        console.log("Sent '%s'", msg);
+        fs.writeFile('../data/11001101', msg, function (err) {
+          if (err) throw err;               console.log('Results Received');
+        }); 
+      });
+      setTimeout(function() {
+        connection.close();
+        process.exit(0)
+      }, 500);  
+
+  }
+
+  function closeOnErr(err) {
+    if (!err) return false;
+    console.error("[AMQP] error", err);
+    amqpConn.close();
+    return true;
+  }
+
+
+  this.stop = function() {
+    amqpConn.close();
+    return true;
+  }
   
 }
 
-function closeOnErr(err) {
-  if (!err) return false;
-  console.error("[AMQP] error", err);
-  amqpConn.close();
-  return true;
-}
-
-
-function stop() {
-  amqpConn.close();
-  return true;
-}
