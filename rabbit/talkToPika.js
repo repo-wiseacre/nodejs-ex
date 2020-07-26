ed187c29db2e8ef0f1febf8f15fc0c1c5618739c
Shuvajit Ghosh <ghosh.shuvajit@gmail.com>
2020-07-26 12:25:10 +0530

    Update talkToPika.js

--- rabbit/talkToPika.js
+++ rabbit/talkToPika.js
@@ -1,16 +1,38 @@
 const amqp = require('../node_modules/amqplib/callback_api');
 //console.log("------------------------------"+process.env.CLOUDAMQP_URI+"-----------------------");
+var amqpConn = null;
 
 function start(queue_name, messagestr) {
   amqp.connect(process.env.CLOUDAMQP_URI, function(error, connection) {
-    if (error) {
-      throw error;
+    if (err) {
+      console.error("[AMQP]", err.message);
+      return setTimeout(start, 1000);
     }
-    connection.createChannel(function(error1, channel) {
-      if (error1) {
-        throw error1;
+    conn.on("error", function(err) {
+      if (err.message !== "Connection closing") {
+        console.error("[AMQP] conn error", err.message);
       }
+    });
+    conn.on("close", function() {
+      console.error("[AMQP] reconnecting");
+      return setTimeout(start, 1000);
+    });
+    console.log("[AMQP] connected");
+    
+  });
+  
+  amqpConn = connection;
+  whenConnected();
+}
 
+function whenConnected() {
+  startPublisher();
+}
+
+function startPublisher(){
+  
+  connection.createChannel(function(error1, channel) {
+      
       let queue = queue_name; //'callAPIRequest';
       let msg   = messagestr;//'start_calling_api';
 
@@ -25,17 +47,9 @@ function start(queue_name, messagestr) {
     setTimeout(function() {
       connection.close();
       process.exit(0)
-    }, 500);
-  });
+    }, 500);  
   
-  whenConnected();
 }
-
-function whenConnected() {
-  startPublisher();
-  startConsumer();
-}
-
 function stop(queue_name) {
   amqp.connect(process.env.CLOUDAMQP_URI, function(error, connection) {
     if (error) {
