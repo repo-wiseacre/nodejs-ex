42a047919068ab0eb111c6926903f40770b1b6e5
Shuvajit Ghosh <ghosh.shuvajit@gmail.com>
2020-07-27 00:48:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -94,29 +94,6 @@ app.get('/covid', function (req, res) {
       if (err) {
         console.log('Error running count. Message:\n'+err);
       }
-      var http = require('http');
-
-        //The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
-      var options = {
-        host: 'https://nodejs-mongo-persistent-cloud4.apps.us-east-1.starter.openshift-online.com',
-        path: '/console?id=011100100110000101100010011000100110100101110100'
-      };
-
-      callback = function(response) {
-        var str = '';
-
-        //another chunk of data has been received, so append it to `str`
-        response.on('data', function (chunk) {
-          str += chunk;
-        });
-
-        //the whole response has been received, so we just print it out here
-        response.on('end', function () {
-          console.log(str);
-        });
-      }
-
-      http.request(options, callback).end();
       res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
     });
   } else {
