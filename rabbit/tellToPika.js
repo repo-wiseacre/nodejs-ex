25958331a8b2337a509df492b8136ac09b2659dc
Shuvajit Ghosh <ghosh.shuvajit@gmail.com>
2020-07-27 14:03:03 +0530

    Update tellToPika.js

--- rabbit/tellToPika.js
+++ rabbit/tellToPika.js
@@ -9,12 +9,12 @@ var tellToPika = {
           console.error("[AMQP]", err.message);
           return setTimeout(start, 1000);
         }
-        conn.on("error", function(err) {
+        connection.on("error", function(err) {
           if (err.message !== "Connection closing") {
             console.error("[AMQP] conn error", err.message);
           }
         });
-        conn.on("close", function() {
+        connection.on("close", function() {
           console.error("[AMQP] reconnecting");
           return setTimeout(start, 1000);
         });
