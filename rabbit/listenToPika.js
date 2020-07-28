6a22f50a1eef9fe97db8bfacfcf99f334495cdb1
Shuvajit Ghosh <ghosh.shuvajit@gmail.com>
2020-07-28 15:21:30 +0530

    Update listenToPika.js

--- rabbit/listenToPika.js
+++ rabbit/listenToPika.js
@@ -2,6 +2,48 @@ const amqp = require('../node_modules/amqplib/callback_api');
 var fs     = require('fs');
 //console.log("------------------------------"+process.env.CLOUDAMQP_URI+"-----------------------");
 var amqpConn = null;
+
+
+function whenConnected(queue_name,messagestr,amqpConn) {
+  startConsumer(queue_name,messagestr,amqpConn);
+}
+
+function closeOnErr(err) {
+  if (!err) return false;
+  console.error("[AMQP] error", err);
+  amqpConn.close();
+  return true;
+}
+
+
+function startConsumer(queue_name,messagestr,amqpConn){
+
+  amqpConn.createChannel(function(error1, channel) {
+
+      let queue = queue_name; //'receiveAPIResponse'; //'callAPIRequest';
+      let msg   = messagestr;//'start_calling_api';
+
+      channel.assertQueue(queue, {
+        durable: true
+      });
+      if (closeOnErr(error1)) return;
+
+      channel.consume(queue, Buffer.from(msg), {
+        persistent: true
+      });
+      console.log("Sent '%s'", msg);
+      fs.writeFile('../src/data/11001101', msg, function (err) {
+        if (err) throw err;               console.log('Results Received');
+      }); 
+    });
+    setTimeout(function() {
+      amqpConn.close();
+      process.exit(0)
+    }, 500);  
+
+}
+
+
 var listenToPika = {
 start:function(queue_name, messagestr,amqpConn) {
   amqp.connect(process.env.CLOUDAMQP_URI, function(err, connection) {
@@ -26,45 +68,6 @@ start:function(queue_name, messagestr,amqpConn) {
   
 },
 
-whenConnected: function(queue_name,messagestr,amqpConn) {
-  startConsumer(queue_name,messagestr,amqpConn);
-},
-
-startConsumer: function(queue_name,messagestr,amqpConn){
-
-  connection.createChannel(function(error1, channel) {
-
-      let queue = queue_name; //'receiveAPIResponse'; //'callAPIRequest';
-      let msg   = messagestr;//'start_calling_api';
-
-      channel.assertQueue(queue, {
-        durable: true
-      });
-      if (closeOnErr(error1)) return;
-
-      channel.consume(queue, Buffer.from(msg), {
-        persistent: true
-      });
-      console.log("Sent '%s'", msg);
-      fs.writeFile('../src/data/11001101', msg, function (err) {
-        if (err) throw err;               console.log('Results Received');
-      }); 
-    });
-    setTimeout(function() {
-      amqpConn.close();
-      process.exit(0)
-    }, 500);  
-
-},
-
-closeOnErr: function(err) {
-  if (!err) return false;
-  console.error("[AMQP] error", err);
-  amqpConn.close();
-  return true;
-},
-
-
 stop:function() {
   if(amqpConn){
     amqpConn.close();
@@ -73,6 +76,8 @@ stop:function() {
 }
   
 }
+
+//listenToPika.start('receiveAPIResponse', 'receiveAPIResponse');
 
 module.exports = listenToPika;
 
