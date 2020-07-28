c6c67b9f46411f9cdb516840819b907477b4f2f1
Shuvajit Ghosh <ghosh.shuvajit@gmail.com>
2020-07-28 12:55:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ app.get('/covid', function (req, res) {
 
         //The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'	
       var options = {	
-        host: 'localhost',
+        host: '127.0.0.1',
         //port: '8080',
         path: '../console'
       };	
